2a68cab365cbc7b6817c87ddf12339852ab28c4f
root <root@SJSR1-0014.skhms.com>
2015-02-25 10:23:30 -0800

    Added README file and shell script for build and run

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-autoreconf
+autoreconf -fvi
 
-CFLAGS="-O2" LIBS="-lpthread -laio"
-
-./configure
+CFLAGS="-O2" LIBS="-lpthread -laio" ./configure
 
 make clean
 
---
+++ src/memaslap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+memaslap -s localhost:11211 -T 4 -c 1024 -t 30m -S 15m -v 1.0
+
+
+
+
+
+
+
+#memaslap -s localhost:11211 -T 1 -c 4 -t 10m -S 5m -w 10m 
+
+#memaslap -s localhost:11211 -T 1 -c 1 -t 1m -S 30s
+
+#sleep 30
+#poweroff
+
